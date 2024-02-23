0266186866363cae6dab3aeb654764579aa90757
h321321 <3218948639@qq.com>
2024-02-23 19:42:36 +0800

    修订paru安装脚本

--- installs/paru.sh
+++ installs/paru.sh
@@ -6,7 +6,7 @@ sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
-read -r input -p ":: 移除安装残留的git包吗[Y/n]: "
+read -p ":: 移除安装残留的git包吗[Y/n]: " input
 if [[ $input = "" ]]; then
   input="Y"
 fi
@@ -14,7 +14,18 @@ fi
 if [[ $input = "Y" ]]; then
   echo ":: 正在移除"
   rm -rf ../paru
+  echo ":: 移除完成"
 fi
 
+read -p ":: 是否下载PKGBUILD高亮 bat[Y/n]: " input2
+if [[ $input2 = "" ]]; then
+  input2="Y"
+fi
+
+if [[ $input2 = "Y" ]]; then
+  echo "bat开始安装"
+  sudo pacman -S bat
+  echo "bat安装完成"
+fi
 echo "安装结束"
 
